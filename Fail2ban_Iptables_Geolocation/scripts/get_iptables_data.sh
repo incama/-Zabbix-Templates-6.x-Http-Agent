ebe09bc29c1ca7d04701b945ce199d74db92aa6e
incama <incama@gmail.com>
2023-03-16 18:55:33 +0100

    First commit

--- Fail2ban_Iptables_Geolocation/scripts/get_iptables_data.sh
+++ Fail2ban_Iptables_Geolocation/scripts/get_iptables_data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Get only the ip addresses
 /sbin/iptables -L -n | awk '$1=="REJECT" && $4!="0.0.0.0/0"' | awk 'match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) {i[substr($0,RSTART,RLENGTH)]=1}END{for(ip in i){printf("%s\n", ip)}}' > /tmp/iptables.txt
